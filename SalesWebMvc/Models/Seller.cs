ba251d38205d313ed0f9d6b87b1f86f08bae2e6d
Tiago Brito <tiago.brito.evangelista@outlook.com>
2019-10-03 16:50:40 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,16 +9,23 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name Required")]
+        [StringLength(60, MinimumLength =3, ErrorMessage = "Name size should be between 3 and 60")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Email Required")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Birth Date Required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "Salary Required")]
+        [Range(998.0, 50000.0, ErrorMessage = "Salary must be from 998.00 to 50.000")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public  double BaseSalary { get; set; }
