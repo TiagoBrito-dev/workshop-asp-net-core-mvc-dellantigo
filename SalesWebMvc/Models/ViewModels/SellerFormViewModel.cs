02ace823a53434193b1ce9af7a8693a64cdd8b7f
Tiago Brito <tiago.brito.evangelista@outlook.com>
2019-10-03 13:35:57 -0300

    Update seller and custom service exception

--- SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -9,5 +9,15 @@ namespace SalesWebMvc.Models.ViewModels
     {
         public Seller Seller { get; set; }
         public ICollection<Department> Departments { get; set; }
+
+        public SellerFormViewModel()
+        {
+        }
+
+        public SellerFormViewModel(Seller seller, ICollection<Department> departments)
+        {
+            Seller = seller;
+            Departments = departments;
+        }
     }
 }
