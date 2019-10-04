00a59a8e4e73c8fd60fb252322133f4add0a59c8
Tiago Brito <tiago.brito.evangelista@outlook.com>
2019-10-04 14:37:08 -0300

    Preparing sales search navigation views

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -42,7 +42,7 @@ namespace SalesWebMvc.Services
                 await _context.SaveChangesAsync();
             }
 
-            catch(DbUpdateException e)
+            catch(DbUpdateException)
             {
                 throw new IntegrityException("can't delete seller because he/she has sales");
             }
